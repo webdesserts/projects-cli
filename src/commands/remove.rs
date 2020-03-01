1dd2c77a69ee53f7cb6f43e5590f29234859ee0a
Michael Mullins <michael@webdesserts.com>
2020-03-01 00:36:23 -0600

    Improve messaging for remove failures

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -1,10 +1,19 @@
 use std::path::{PathBuf};
-use dialoguer::Select;
+use dialoguer;
 use crate::config::{Config};
 use crate::utils;
 use crate::commands::list::{list_roots};
 
 pub fn remove(path: Option<PathBuf>, mut config: Config) -> Result<(), failure::Error> {
+    if path == None && config.paths.is_empty() {
+        println!("You are not currently tracking any directories. You can track projects in a");
+        println!("directory by using the `projects track` command:");
+        println!("");
+        println!("  projects track ~/code/");
+        println!("");
+        return Ok(())
+    }
+
     let p = match path {
         Some(path) => path,
         None => select_root(&config)?
@@ -25,11 +34,12 @@ pub fn remove(path: Option<PathBuf>, mut config: Config) -> Result<(), failure::
  * @todo what if there are no projects?
  */
 pub fn select_root(config: &Config) -> Result<PathBuf, failure::Error> {
+    println!("You are currently tracking projects in the following directories. Which would you like to remove?");
     let rootset = list_roots(config)?;
     let mut roots: Vec<&PathBuf> = rootset.iter().collect();
     roots.sort();
     let path_strings: Vec<&str> = roots.iter().map(|root| root.to_str().unwrap()).collect();
-    let mut selector = Select::new();
+    let mut selector = dialoguer::Select::new();
     selector.items(&path_strings);
     selector.default(0);
     let result = selector.interact()?;
